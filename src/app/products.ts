d7c1d4a8d10a15e4f9bf3b5f1505c8c4429d8f39
Jeremy Wang <jdw0134@auburn.edu>
2024-02-03 16:12:40 -0600

    Product Line Version 1

--- src/app/products.ts
+++ src/app/products.ts
@@ -7,23 +7,310 @@ export interface Product {
 
 export const products = [
   {
-    id: 1,
+    id: 0,
     name: 'Phone XL',
     price: 799,
     description: 'A large phone with one of the best screens'
   },
   {
-    id: 2,
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras'
+    id: 11,
+    type: 'Nose',
+    name: 'Blunt Nose',
+    price: 0,
+    weight: 0,
+    aero: null,
+    turn: null,
+    capcity: null,
+    thrust: null,
+    efficiency: null,
+    fuel: null,
+    description: 'Blunt'
   },
   {
-    id: 3,
-    name: 'Phone Standard',
-    price: 299,
-    description: ''
+    id: 12,
+    type: 'Nose',
+    name: 'Parabolic Nose',
+    price: 0,
+    weight: 0,
+    aero: null,
+    turn: null,
+    capcity: null,
+    thrust: null,
+    efficiency: null,
+    fuel: null,
+    description: 'Parabolic'
   }
+  ,
+  {
+    id: 13,
+    type: 'Nose',
+    name: 'Ogive Nose',
+    price: 0,
+    weight: 0,
+    aero: null,
+    turn: null,
+    capcity: null,
+    thrust: null,
+    efficiency: null,
+    fuel: null,
+    description: 'Ogive'
+  }
+  ,
+  {
+    id: 14,
+    type: 'Nose',
+    name: 'Cone Nose',
+    price: 0,
+    weight: 0,
+    aero: null,
+    turn: null,
+    capcity: null,
+    thrust: null,
+    efficiency: null,
+    fuel: null,
+    description: 'Cone'
+  }
+  ,
+  {
+    id: 21,
+    type: 'Crew'
+    name: 'No Crew',
+    price: 0,
+    weight: 0,
+    aero: null,
+    turn: null,
+    capcity: null,
+    thrust: null,
+    efficiency: null,
+    fuel: null,
+    description: 'No Crew'
+  }
+  ,
+  {
+    id: 22,
+    type: 'Crew'
+    name: '2 Crew',
+    price: 0,
+    weight: 0,
+    aero: null,
+    turn: null,
+    capcity: null,
+    thrust: null,
+    efficiency: null,
+    fuel: null,
+    description: '2 Crew'
+  }
+  ,
+  {
+    id: 23,
+    type: 'Crew'
+    name: '3 Crew',
+    price: 0,
+    weight: 0,
+    aero: null,
+    turn: null,
+    capcity: null,
+    thrust: null,
+    efficiency: null,
+    fuel: null,
+    description: '3 Crew'
+  }
+  ,
+  {
+    id: 24,
+    type: 'Crew'
+    name: '5 Crew',
+    price: 0,
+    weight: 0,
+    aero: null,
+    turn: null,
+    capcity: null,
+    thrust: null,
+    efficiency: null,
+    fuel: null,
+    description: '5 Crew'
+  }
+  ,
+  {
+    id: 31,
+    type: 'Cargo'
+    name: 'Specialized Cargo',
+    price: 0,
+    weight: 0,
+    aero: null,
+    turn: null,
+    capcity: null,
+    thrust: null,
+    efficiency: null,
+    fuel: null,
+    description: 'Specialized Cargo'
+  }
+  ,
+  {
+    id: 32,
+    type: 'Cargo'
+    name: 'No Cargo',
+    price: 0,
+    weight: 0,
+    aero: null,
+    turn: null,
+    capcity: null,
+    thrust: null,
+    efficiency: null,
+    fuel: null,
+    description: 'No Cargo'
+  }
+  ,
+  {
+    id: 33,
+    type: 'Cargo'
+    name: 'Small Cargo',
+    price: 0,
+    weight: 0,
+    aero: null,
+    turn: null,
+    capcity: null,
+    thrust: null,
+    efficiency: null,
+    fuel: null,
+    description: 'Small Cargo'
+  }
+  ,
+  {
+    id: 34,
+    type: 'Cargo'
+    name: 'Medium Cargo',
+    price: 0,
+    weight: 0,
+    aero: null,
+    turn: null,
+    capcity: null,
+    thrust: null,
+    efficiency: null,
+    fuel: null,
+    description: 'Medium Cargo'
+  }
+  ,
+  {
+    id: 41,
+    type: 'Engine'
+    name: 'Small Solid Fuel Engine',
+    price: 0,
+    weight: 0,
+    aero: null,
+    turn: null,
+    capcity: null,
+    thrust: null,
+    efficiency: null,
+    fuel: null,
+    description: 'Small Solid Fuel Engine'
+  } 
+  ,
+  {
+    id: 42,
+    type: 'Engine'
+    name: 'Large Solid Fuel Engine',
+    price: 0,
+    weight: 0,
+    aero: null,
+    turn: null,
+    capcity: null,
+    thrust: null,
+    efficiency: null,
+    fuel: null,
+    description: 'Large Solid Fuel Engine'
+  }
+  ,
+  {
+    id: 43,
+    type: 'Engine'
+    name: 'Small Liquid Fuel Engine',
+    price: 0,
+    weight: 0,
+    aero: null,
+    turn: null,
+    capcity: null,
+    thrust: null,
+    efficiency: null,
+    fuel: null,
+    description: 'Small Liquid Fuel Engine'
+  }
+  ,
+  {
+    id: 44,
+    type: 'Engine'
+    name: 'Large Liquid Fuel Engine',
+    price: 0,
+    weight: 0,
+    aero: null,
+    turn: null,
+    capcity: null,
+    thrust: null,
+    efficiency: null,
+    fuel: null,
+    description: 'Large Liquid Fuel Engine'
+  }
+  ,
+  {
+    id: 51,
+    type: 'Fin'
+    name: 'Clipped Delta Fin',
+    price: 0,
+    weight: 0,
+    aero: null,
+    turn: null,
+    capcity: null,
+    thrust: null,
+    efficiency: null,
+    fuel: null,
+    description: 'Clipped Delta Fin'
+  }
+  ,
+  {
+    id: 52,
+    type: 'Fin'
+    name: 'Trapezoidal Fin',
+    price: 0,
+    weight: 0,
+    aero: null,
+    turn: null,
+    capcity: null,
+    thrust: null,
+    efficiency: null,
+    fuel: null,
+    description: 'Trapezodial Fin'
+  }
+  ,
+  {
+    id: 53,
+    type: 'Fin'
+    name: 'Tapered Swept Fin',
+    price: 0,
+    weight: 0,
+    aero: null,
+    turn: null,
+    capcity: null,
+    thrust: null,
+    efficiency: null,
+    fuel: null,
+    description: 'Tapered Swept Fin'
+  }
+  ,
+  {
+    id: 54,
+    type: 'Fin'
+    name: 'Elliptical Fin',
+    price: 0,
+    weight: 0,
+    aero: null,
+    turn: null,
+    capcity: null,
+    thrust: null,
+    efficiency: null,
+    fuel: null,
+    description: 'Elliptical Fin'
+  }
+
 ];
 
 
