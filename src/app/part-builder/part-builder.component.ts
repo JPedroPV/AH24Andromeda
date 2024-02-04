2db00e37b10033c5abc658b5db9424bff9783751
Pedro Vasconcelos <jpv0016@auburn.edu>
2024-02-03 20:42:29 -0600

    Initial Builder

---
+++ src/app/part-builder/part-builder.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-part-builder',
+  templateUrl: './part-builder.component.html',
+  styleUrl: './part-builder.component.css'
+})
+export class PartBuilderComponent {
+
+}
